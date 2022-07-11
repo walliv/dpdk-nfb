b08d95818b9c23086e00b3e7276d7e816f55b0b5
Martin Spinler <spinler@cesnet.cz>
2022-07-11 11:52:11 +0200

    drivers/nfb: add mdio.c, mdio.h with IEEE802.3 functions
    Add DPDK specific functions for obtaining informations from
    standardized MDIO interface via NFB specific mdio_if_info
    callback.

---
+++ drivers/net/nfb/mdio.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 CESNET, z.s.p.o.
+ * All rights reserved.
+ */
+
+#include <netcope/mdio_if_info.h>
+
+void
+nfb_ieee802_3_pma_pmd_get_speed_capa(struct mdio_if_info *if_info, uint32_t *capa);
