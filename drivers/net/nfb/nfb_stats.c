5adc43fcb6c26fcff399cb88f44e47f51c49eb37
Kamil Vojanec <vojanec@cesnet.cz>
2023-06-22 14:01:06 +0200

    drivers/nfb: fix garbage statistics due to wrong pointer dereference
    The tx_queue and rx_queue pointer was incorrectly indexed, leading to
    garbage data being returned as dev_stats.

--- drivers/net/nfb/nfb_stats.c
+++ drivers/net/nfb/nfb_stats.c
@@ -23,32 +23,32 @@ nfb_eth_stats_get(struct rte_eth_dev *dev,
 	if (dev->data == NULL)
 		return -ENOMEM;
 
-	struct ndp_rx_queue *rx_queue = NULL;
-	struct ndp_tx_queue *tx_queue = NULL;
+	struct ndp_rx_queue **rx_queue = NULL;
+	struct ndp_tx_queue **tx_queue = NULL;
 
 	if (dev->data->rx_queues) {
-		rx_queue = *((struct ndp_rx_queue **) dev->data->rx_queues);
+		rx_queue = (struct ndp_rx_queue **) dev->data->rx_queues;
 
 		for (i = 0; i < nb_rx; i++) {
 			if (i < RTE_ETHDEV_QUEUE_STAT_CNTRS) {
-				stats->q_ipackets[i] = rx_queue[i].rx_pkts;
-				stats->q_ibytes[i] = rx_queue[i].rx_bytes;
+				stats->q_ipackets[i] = rx_queue[i]->rx_pkts;
+				stats->q_ibytes[i] = rx_queue[i]->rx_bytes;
 			}
-			rx_total += rx_queue[i].rx_pkts;
-			rx_total_bytes += rx_queue[i].rx_bytes;
+			rx_total += rx_queue[i]->rx_pkts;
+			rx_total_bytes += rx_queue[i]->rx_bytes;
 		}
 	}
 
 	if (dev->data->tx_queues) {
-		tx_queue = *((struct ndp_tx_queue **) dev->data->tx_queues);
+		tx_queue = (struct ndp_tx_queue **) dev->data->tx_queues;
 		for (i = 0; i < nb_tx; i++) {
 			if (i < RTE_ETHDEV_QUEUE_STAT_CNTRS) {
-				stats->q_opackets[i] = tx_queue[i].tx_pkts;
-				stats->q_obytes[i] = tx_queue[i].tx_bytes;
+				stats->q_opackets[i] = tx_queue[i]->tx_pkts;
+				stats->q_obytes[i] = tx_queue[i]->tx_bytes;
 			}
-			tx_total += tx_queue[i].tx_pkts;
-			tx_total_bytes += tx_queue[i].tx_bytes;
-			tx_err_total += tx_queue[i].err_pkts;
+			tx_total += tx_queue[i]->tx_pkts;
+			tx_total_bytes += tx_queue[i]->tx_bytes;
+			tx_err_total += tx_queue[i]->err_pkts;
 		}
 	}
 
@@ -67,20 +67,20 @@ nfb_eth_stats_reset(struct rte_eth_dev *dev)
 	uint16_t nb_rx = dev->data->nb_rx_queues;
 	uint16_t nb_tx = dev->data->nb_tx_queues;
 
-	struct ndp_rx_queue *rx_queue = *((struct ndp_rx_queue **)
-		dev->data->rx_queues);
-	struct ndp_tx_queue *tx_queue = *((struct ndp_tx_queue **)
-		dev->data->tx_queues);
+	struct ndp_rx_queue **rx_queue = (struct ndp_rx_queue **)
+		dev->data->rx_queues;
+	struct ndp_tx_queue **tx_queue = (struct ndp_tx_queue **)
+		dev->data->tx_queues;
 
 	for (i = 0; i < nb_rx; i++) {
-		rx_queue[i].rx_pkts = 0;
-		rx_queue[i].rx_bytes = 0;
-		rx_queue[i].err_pkts = 0;
+		rx_queue[i]->rx_pkts = 0;
+		rx_queue[i]->rx_bytes = 0;
+		rx_queue[i]->err_pkts = 0;
 	}
 	for (i = 0; i < nb_tx; i++) {
-		tx_queue[i].tx_pkts = 0;
-		tx_queue[i].tx_bytes = 0;
-		tx_queue[i].err_pkts = 0;
+		tx_queue[i]->tx_pkts = 0;
+		tx_queue[i]->tx_bytes = 0;
+		tx_queue[i]->err_pkts = 0;
 	}
 
 	return 0;
